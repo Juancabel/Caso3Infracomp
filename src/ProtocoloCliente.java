9ae0fd44f98d280007022f20513c12efd545c66f
Juan Beltran <jc.beltrang1@uniandes.edu.co>
2024-05-05 14:54:30 -0500

    RSA funcional

--- src/ProtocoloCliente.java
+++ src/ProtocoloCliente.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.Base64;
 import java.util.Random;
 import java.security.*;
 import javax.crypto.*;
@@ -25,13 +26,15 @@ public class ProtocoloCliente {
             throw new Exception("Algo sucedio");
         }
     
-
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
         PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
 
         //Recibe el mensaje del usuario
         System.out.println("Comenzando Proceso...");
-        System.out.println(privateKey);
+        String privKey = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+        
+        pOut.println(privKey);
 
     }
 }
--- src/ProtocoloServidor.java
+++ src/ProtocoloServidor.java
@@ -2,7 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.Base64;
 import java.util.Random;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+import javax.crypto.*;
 
 public class ProtocoloServidor {
 
@@ -17,7 +22,21 @@ public class ProtocoloServidor {
 
     public static void procesar(BufferedReader pIn, PrintWriter pOut) throws IOException,Exception{
         
+        String privKey = pIn.readLine();
+        PrivateKey privateKey = null;
+        
+        try {
+            String privateK = privKey;
+            byte[] privateBytes = Base64.getDecoder().decode(privateK);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            privateKey = keyFactory.generatePrivate(keySpec);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        System.out.println(privateKey);
 
-
+        
+        
     }
 }
